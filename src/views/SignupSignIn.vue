6a54a373fccffb9266f980ff18aba063e3c156c1
Waqar Shaikh <waqarshaikh789@gmail.com>
2024-03-11 23:16:55 +0530

    added styling to all pages

--- src/views/SignupSignIn.vue
+++ src/views/SignupSignIn.vue
@@ -2,13 +2,13 @@
   <div>
     <!-- Modal -->
     <div class="modal fade" id="authModal" tabindex="-1" aria-labelledby="authModalLabel" aria-hidden="true">
-      <div class="modal-dialog d-flex align-items-center">
-        <div class="modal-content">
-          <div class="modal-header">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content bg-dark text-light">
+          <div class="modal-header border-0">
             <h5 class="modal-title" id="authModalLabel">Authentication</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body d-flex flex-column align-items-center ">
+          <div class="modal-body">
             <!-- Tabs for Sign In and Sign Up -->
             <ul class="nav nav-tabs" id="authTabs">
               <li class="nav-item">
@@ -33,8 +33,8 @@
               </div>
             </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <div class="modal-footer border-0">
+            <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Close</button>
           </div>
         </div>
       </div>
@@ -52,12 +52,54 @@ onMounted(() => {
   const authModal = new bootstrap.Modal(document.getElementById('authModal'));
   authModal.show();
 });
+
 const authSuccess = (user: any) => {
   // Handle the event here
   console.log('Authentication successful');
   console.log('Authentication successful', user);
   const authModal = new bootstrap.Modal(document.getElementById('authModal'));
-  store.commit('setUser', user)
+  store.commit('setUser', user);
   authModal.hide();
 }
 </script>
+
+<style scoped>
+.modal-content {
+  border: 2px solid #28a745;
+}
+
+.nav-tabs .nav-link {
+  color: #fff;
+}
+
+.nav-tabs .nav-link.active {
+  background-color: #28a745;
+  border-color: #28a745;
+}
+
+.modal-header {
+  background-color: #343a40;
+}
+
+.modal-body {
+  background-color: #343a40;
+}
+
+.modal-footer {
+  background-color: #343a40;
+}
+
+.btn-secondary {
+  background-color: #6c757d;
+  border-color: #6c757d;
+}
+
+.btn-secondary:hover {
+  background-color: #5a6268;
+  border-color: #5a6268;
+}
+
+.btn-close-white {
+  color: #fff;
+}
+</style>
