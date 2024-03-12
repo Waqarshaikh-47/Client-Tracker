c0c9ba75f43a632c9cac9a0394b223c18c8ac34d
nimit0703 <nimit.190410107143@gmail.com>
2024-03-12 21:47:41 +0530

    clientInfoSchema added

---
+++ src/schemas/forms/ClientInformation.ts
@@ -0,0 +1,17 @@
+// ClientInformation.ts
+export class ClientInformation {
+    fullName: string;
+    panNumber: string;
+    dob: string;
+    email: string;
+    phone: string;
+  
+    constructor(fullName: string, panNumber: string, dob: string, email: string, phone: string) {
+      this.fullName = fullName;
+      this.panNumber = panNumber;
+      this.dob = dob;
+      this.email = email;
+      this.phone = phone;
+    }
+  }
+  
